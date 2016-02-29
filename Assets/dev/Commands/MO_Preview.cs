298421720e67b7816561c380bb22e456827d0d24
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2016-03-01 00:48:33 +0100

    remove *.meta from gitignore and add all meta objects to the repo

---
+++ Assets/dev/Commands/MO_Preview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class MO_Preview : Command
+{
+    
+ public MO_Preview (Actor actor) : base (actor)
+ {
+ }
+
+ public override bool IsExecuteable ()
+ {
+  return true;
+ }
+
+    
+ public override void Execute ()
+ {
+  Debug.Log ("Executed");
+    
+ }
+
+ public override void AfterExecution ()
+ {
+  // initialize delayed execution parameter
+  this.m_Frames = 0f;
+ }
+
+ public override void ExecuteDelayed ()
+ {
+  Debug.Log ("ExecutedDelayed " + m_Owner.T);
+ }
+    
+}
+
+
