07e715c7568df99aa73b5ba29e65907bf26f0a99
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2016-03-01 12:59:16 +0100

    inherit singeltons CommandStack from Stack and CommandQueue from Queue

--- Assets/dev/CommandHandling/CommandStack.cs
+++ Assets/dev/CommandHandling/CommandStack.cs
@@ -2,10 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CommandStack : MonoBehaviour {
+public class CommandStack : Stack<Command>
+{
 
-  public Stack<Command> m_CommandStack;
 
+ private CommandStack () : base ()
+ {
+ }
 
-	
+ private static CommandStack m_Instance;
+
+ public static CommandStack Instance {
+  get {
+   if (m_Instance == null) {
+    m_Instance = new CommandStack ();
+   }
+   return m_Instance;
+
+  }
+ }
+
+ public void ProcessUndoStack ()
+ {
+  if (this.Count > 0) {
+   var undo_cmd = this.Pop ();
+   undo_cmd.BeforeUndoExecution();
+   undo_cmd.UndoExecution ();
+   undo_cmd.AfterUndoExecution();
+  }
+ }
+
+      	
 }
