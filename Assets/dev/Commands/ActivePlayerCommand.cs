0ee9f7431c8d848a5fa3437220a37beafc1748eb
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2016-03-01 13:02:49 +0100

    simple set ActivePlayerCommand updated

--- Assets/dev/Commands/ActivePlayerCommand.cs
+++ Assets/dev/Commands/ActivePlayerCommand.cs
@@ -11,22 +11,31 @@ public class ActivePlayerCommand : Command
  }
 
  public override bool IsExecuteable ()
-  {
-    return m_Owner.T != Actor.Type.Tile;
-  }
+ {
+  return m_Owner.T != Actor.Type.Tile;
+ }
 
  public override void Execute ()
  {
   // save previouse actor for undo/action
   this.m_PreviouseActor = m_LifeCycle.ActiveActor;
+
+  if (this.m_PreviouseActor)
+   this.m_PreviouseActor.Appearence.GetComponent<MeshRenderer> ().material.color = Color.white;
+
   // set the new active actor
   m_LifeCycle.ActiveActor = m_Owner;
+  if(m_Owner)
+    m_Owner.Appearence.GetComponent<MeshRenderer> ().material.color = Color.blue;
  }
 
  public override void UndoExecution ()
  {
- // reset previouse actor
-  m_LifeCycle.ActiveActor = m_PreviouseActor;
+  // reset previouse actor
+  var cmd = new ActivePlayerCommand (m_PreviouseActor);
+  cmd.Execute ();
+  // do not add to command queue
+  // 
  }
 
 
